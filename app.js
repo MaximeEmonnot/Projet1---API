3fd969522db81fb09dba2981ff14ca18e0125534
benji4411 <mahoudeaubenjamin@gmail.com>
2022-10-29 17:42:26 +0200

    Ajout de la requête hello

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const jwt = require("jsonwebtoken");
 const express = require("express");
+
+const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
+app.listen(3000, () => {
+  console.log("Attente des requêtes au port 3000");
+});
